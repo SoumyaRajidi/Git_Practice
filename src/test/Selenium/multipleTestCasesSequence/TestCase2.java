cf4a15c73fedfc88ea0958d85d6313866b34d081
Soumya Rajidi <soumya83.rajidi@gmail.com>
2019-11-01 11:11:25 +0000

    changes removed

--- src/test/Selenium/multipleTestCasesSequence/TestCase2.java
+++ src/test/Selenium/multipleTestCasesSequence/TestCase2.java
@@ -8,8 +8,6 @@ public class TestCase2 extends testbase2{
 	@Test
 	public void test2(){
 		System.out.println("thisis testcase2");
-		System.out.println("this is just for testing git");
-		System.out.println(" git ");
 	}
 
 }
